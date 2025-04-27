6e5661c1f55091c485ec99e388dcae6962229e54
3ch0v0 <lijiaqi.echo@gmail.com>
2025-04-27 09:27:45 +0100

    add UI assets

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,18 +4,39 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    public GameObject stageContent;
     public GameObject destination;            
     private int totalStars;
     public int collectedStars = 0;
     private bool destinationActive = false;
     public PlayerController player;
+    
+    private UIController uiController;
+    private bool stageCompleted = false;
 
     void Start()
     {
+        uiController = FindObjectOfType<UIController>();
         destination?.SetActive(false); 
-        
         totalStars = GameObject.FindGameObjectsWithTag("Star").Length;
         
+        if (stageContent != null)
+            stageContent.SetActive(false); // 一开始隐藏地图内容
+
+        if (SceneManager.GetActiveScene().buildIndex != 0) // 如果不是菜单
+        {
+            if (uiController != null)
+            {
+                uiController.ShowStageUI(SceneManager.GetActiveScene().buildIndex);
+                Invoke(nameof(ShowLevelContent), 2f); // 2秒后显示地图
+            }
+        }
+        else
+        {
+            // 如果是菜单Scene（BuildIndex=0），直接显示levelContent（比如背景动画）
+            if (stageContent != null)
+                stageContent.SetActive(true);
+        }
     }
 
     void Update()
@@ -52,16 +73,35 @@ public class LevelManager : MonoBehaviour
 
         if (currentIndex < totalScenes - 1)
         {
-            SceneManager.LoadScene(currentIndex + 1);
+            StartCoroutine(LoadNextStage(currentIndex + 1));
         }
         else
         {
-            Debug.Log("Game completed");
+            if (uiController != null)
+            {
+                uiController.ShowStageClear();
+            }
         }
     }
     
     public void RestartLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    
+    void ShowLevelContent()
+    {
+        if (stageContent != null)
+            stageContent.SetActive(true);
+    }
+    private System.Collections.IEnumerator LoadNextStage(int nextIndex)
+    {
+        if (uiController != null)
+        {
+            uiController.ShowStageUI(nextIndex); // 切关时也弹出新的Stage UI
+            yield return new WaitForSeconds(2f); // 等2秒
+        }
+
+        SceneManager.LoadScene(nextIndex);
     }
 }
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,20 +1,93 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
+    [Header("GravityNum UI")]
     public TextMeshProUGUI gravityNumText;
     public PlayerController pCScripts;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    [Header("Starting UI")]
+    public GameObject pressAnyKeyPanel;
 
+    [Header("Stage UIs")]
+    public GameObject[] stageUIs; 
+    public GameObject stageClearUI; 
+
+    private bool gameStarted = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0) // 主界面才启用Press Any Key
+        {
+            pressAnyKeyPanel?.SetActive(true);
+        }
+        else
+        {
+            pressAnyKeyPanel?.SetActive(false);
+            HideAllStageUIs();
+            ShowStageUI(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
+        if (SceneManager.GetActiveScene().buildIndex == 0 && !gameStarted)
+        {
+            if (Input.anyKeyDown)
+            {
+                gameStarted = true;
+                pressAnyKeyPanel?.SetActive(false);
+                LoadFirstStage();
+            }
+        }
         GetGravityNum();
     }
 
     void GetGravityNum()
     {
         gravityNumText.text="Gravity Number: "+pCScripts.gravityNum;
+    }
+    
+    void LoadFirstStage()
+    {
+        SceneManager.LoadScene(1); // 跳到BuildIndex 1的第一关
+    }
+    
+    public void ShowStageUI(int stageIndex)
+    {
+        HideAllStageUIs();
+
+        if (stageIndex - 1 < stageUIs.Length && stageIndex > 0)
+        {
+            stageUIs[stageIndex - 1].SetActive(true);
+            Invoke(nameof(HideAllStageUIs), 2f); 
+        }
+        else
+        {
+            Debug.LogWarning("Stage index out of range.");
+        }
+    }
+
+    public void ShowStageClear()
+    {
+        HideAllStageUIs();
+        if (stageClearUI != null)
+        {
+            stageClearUI.SetActive(true);
+        }
+    }
+
+    void HideAllStageUIs()
+    {
+        foreach (var ui in stageUIs)
+        {
+            if (ui != null)
+                ui.SetActive(false);
+        }
+
+        if (stageClearUI != null)
+            stageClearUI.SetActive(false);
     }
 }
